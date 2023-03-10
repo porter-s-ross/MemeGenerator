28a8be670819384b9091bb2bf52bdcf67e651bb1
rss <opqlns@pm.me>
2023-03-09 22:43:32 -0600

    Added useState to randomly generate an image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import memesData from '../memesData'
 
 function Meme(){
+
+    const [Meme, setMeme] = React.useState("");
+
+    function onClickHandler(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+        setMeme(url)
+    }
+
     return(
     <main>
-        <form className="meme--form">
+        <div className="meme--form">
             <input type="text" className="input--control" placeholder="Shut Up"/>
             <input type="text" className="input--control" placeholder="and take my money"/>
-            <button className="form--button">
+            <button onClick={onClickHandler} className="form--button">
                 Get a new meme image 🖼
             </button>
-        </form>
+        </div>
+        <img src={Meme} alt="Generated Meme Image" />
     </main>  
     )
 }
