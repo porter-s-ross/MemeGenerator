52eb5ab7f38bb45e4df175a3e93f941f4ba4808c
rss <opqlns@pm.me>
2023-03-09 19:16:48 -0600

    init commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div className="header">
+            <div className="header-left">
+                <img src="src/assets/troll-face.png" alt="" />
+                <h1 className="title">Meme Generator</h1>
+            </div>
+            <div className="header-right">
+            </div>
+        </div>
+    )
+}
+
+export default Header
