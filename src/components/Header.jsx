66c05d50ee049713891e398b14b0f7f023f8a964
rss <opqlns@pm.me>
2023-03-23 22:38:11 -0500

    changed headerimg source to correctly import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import trollFace from "../assets/troll-face.png"
 
 function Header(){
     return(
         <header className="header">
-            <img src="src/assets/troll-face.png" alt="Troll Face" className="header--image"/>
+            <img src={trollFace} alt="Troll Face" className="header--image"/>
             <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--project">React Course - Project</h4>
         </header>
